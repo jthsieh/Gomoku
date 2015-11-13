c26deb9c3ad53a29a013bb5e352d7dcf76d2e7dc
Tim Hsieh <junting@stanford.edu>
2015-11-12 21:10:15 -0800

    First commit.

---
+++ gameState.py
@@ -0,0 +1,8 @@
+class GameState():
+    # player: 0, 1
+    # Return -1 if false, otherwise return the winner's number
+    def gameEnded():
+        return 0
+
+    def moveIsValid(player, move):
+        return True
