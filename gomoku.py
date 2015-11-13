a24b4b252d873c13f8efc22b9046b270a05d524b
Sherwin <sherwinxyy@gmail.com>
2015-11-12 21:35:44 -0800

    sha dou mei gan

--- gomoku.py
+++ gomoku.py
@@ -1,2 +1,19 @@
-def __name__ = '__main__':
+import random
+import os
 
+def runGames():
+   # import __main__
+    print('Welcome to our Gomoku game for CS221')
+   # while True:
+   #    print boardSize 
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+#      args = readCommand( sys.argv[1:] ) # Get game components based on input
+      runGames()
