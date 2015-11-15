228eb11194b51651da5e6c4e333c11a04d269181
Tim Hsieh <junting@stanford.edu>
2015-11-15 15:29:41 -0800

    Finished features. Features are stored in gameState.features and represented as ((player, feature): number) pair. It will be updated on every move.

--- gameState.py
+++ gameState.py
@@ -1,3 +1,5 @@
+import util
+
 class GameState():
     """
     This class contains information of the state of a Gomoku game.
@@ -9,6 +11,9 @@ class GameState():
          numPlayers: How many total players in this game (including computers)
 
     There are N players.
+
+    Features: (agentIndex, description) => number
+    Ex. (agentIndex, 'open 3') => 2
     """
 
     def __init__(self, N, boardSize, numPlayers, prevState = None):
@@ -19,10 +24,12 @@ class GameState():
             self.board = {}
             self.gameOver = False
             self.winner = -1
+            self.features = {} # Ex. (agentIndex, 'open 3') -> 2
         else:
             self.board = dict(prevState.board)
             self.gameOver = prevState.gameOver
             self.winner = prevState.winner
+            self.features = prevState.features
 
     def getLegalActions(self):
         legalActions = []
@@ -75,18 +82,18 @@ class GameState():
             return
         
         self.board[move] = player
-        if self.checkWinCondition(player, move):
+        self.updateFeaturesForMove(player, move)
+        if self.checkTie():
             self.gameOver = True
-            self.winner = player
-        elif self.checkTie():
-            self.gameOver = True
+        print str(self)
+        print self.features
 
     def __str__(self):
         s = ''
         for y in range(self.boardSize):
             for x in range(self.boardSize):
                 if (x, y) not in self.board:
-                    s += ' + |'
+                    s += '   |'
                 else:
                     s += ' ' + str(self.board[(x,y)]) + ' |'
             s += "\n" + (4 * self.boardSize * "-") +'\n'
@@ -102,67 +109,123 @@ class GameState():
         else:
             return False
 
-    def checkWinCondition(self, player, move):
+
+    def updateFeaturesForMove(self, player, move):
         """
-        Return True if the player makes a move and wins the game.
+        Update the features when a player makes a move.
+        There are 4 directions (Horizontal, Vertical, Diagonal /, Diagonal \).
         """
         x, y = move
 
-        # Horizontal
+        ############ Horizontal ############
+        neighbors1 = self.checkNeighboringRows(move, [(i, y) for i in range(x + 1, self.boardSize + 1)])
+        neighbors2 = self.checkNeighboringRows(move, [(i, y) for i in range(x - 1, -2, -1)])
+        self.updateFeature(player, move, neighbors1, neighbors2)
+
+        ############ Vertical ############
+        neighbors1 = self.checkNeighboringRows(move, [(x, j) for j in range(y + 1, self.boardSize + 1)])
+        neighbors2 = self.checkNeighboringRows(move, [(x, j) for j in range(y - 1, -2, -1)])
+        self.updateFeature(player, move, neighbors1, neighbors2)
+
+        ############ Diagonal / ############
+        neighbors1 = self.checkNeighboringRows(move, [(x + i, y + i) for i in range(1, self.boardSize + 1)])
+        neighbors2 = self.checkNeighboringRows(move, [(x - i, y - i) for i in range(1, self.boardSize + 1)])
+        self.updateFeature(player, move, neighbors1, neighbors2)
+
+        ############ Diagonal \ ############
+        neighbors1 = self.checkNeighboringRows(move, [(x + i, y - i) for i in range(1, self.boardSize + 1)])
+        neighbors2 = self.checkNeighboringRows(move, [(x - i, y + i) for i in range(1, self.boardSize + 1)])
+        self.updateFeature(player, move, neighbors1, neighbors2)
+
+    def checkNeighboringRows(self, move, piecesRange):
+        """
+        Return (player, num in a row, blocked or not)
+        This logic is complicated...
+        """
+        # Hit boundary (no neighbors)
+        if len(piecesRange) == 0 or not self.withinBounds(piecesRange[0]):
+            return (-1, 0, 1)
+        # Open end (no neighbors)
+        if piecesRange[0] not in self.board:
+            return (-1, 0, 0)
+
+        player = self.board[piecesRange[0]]
+        num = 0
+        blocked = 0
+        for piece in piecesRange:
+            if not self.withinBounds(piece) or (piece in self.board and self.board[piece] != player):
+                blocked = 1
+                break
+            if piece not in self.board:
+                break
+            num += 1
+        return (player, num, blocked)
+
+    def updateFeature(self, player, move, neighbors1, neighbors2):
+        """
+        Delete/Update the features of the neighbors.
+        For example, if neighbor is an open 3 from a different player,
+        then update it to be a blocked 3.
+
+        Then, add a new feature according to the given move and its neighbors.
+        Parameters: neighbors: (playerIndex, num, blocked)
+        """
+        if neighbors1[1] == 0 and neighbors2[1] == 0:
+            return
+
+        print neighbors1, neighbors2
+        # Delete/Update the features of the neighbors
+        for neighbor in [neighbors1, neighbors2]:
+            num = neighbor[1]
+            if neighbor[0] == -1:
+                # no neighbor
+                continue
+            if neighbor[0] != player:
+                # neighbor is a different player, so delete or update the feature
+                if num >= 2:
+                    if neighbor[2] == 0: # not blocked
+                        featureToDelete = (neighbor[0], 'open ' + str(num))
+                        util.deleteItemFromDict(self.features, featureToDelete)
+                        featureToAdd = (neighbor[0], 'blocked ' + str(num))
+                        util.addItemToDict(self.features, featureToAdd)
+                    else: # blocked
+                        featureToDelete = (neighbor[0], 'blocked ' + str(num))
+                        util.deleteItemFromDict(self.features, featureToDelete)
+            else:
+                # neighbor is the same player, so delete the feature
+                if num >= 2:
+                    if neighbor[2] == 0: # not blocked
+                        featureToDelete = (neighbor[0], 'open ' + str(num))
+                        util.deleteItemFromDict(self.features, featureToDelete)
+                    else: # blocked
+                        featureToDelete = (neighbor[0], 'blocked ' + str(num))
+                        util.deleteItemFromDict(self.features, featureToDelete)
+
+        # Add a new feature according to the given move and its neighbors.
         num = 1
-        for i in range(x + 1, self.boardSize):
-            if not (i, y) in self.board or self.board[(i, y)] != player:
-                break
-            num += 1
-        for i in range(x - 1, -1, -1):
-            if not (i, y) in self.board or self.board[(i, y)] != player:
-                break
-            num += 1
+        blocked = 0
+        for neighbor in [neighbors1, neighbors2]:
+            if neighbor[0] == -1:
+                blocked += neighbor[2]
+            elif neighbor[0] == player:
+                num += neighbor[1]
+                blocked += neighbor[2]
+            else:
+                blocked += 1
+
+        # Game ends if num >= self.N
         if num >= self.N:
-            return True
+            self.gameOver = True
+            self.winner = player
 
-        # Vertical
-        num = 1
-        for j in range(y + 1, self.boardSize):
-            if (x, j) not in self.board or self.board[(x, j)] != player:
-                break
-            num += 1
-        for j in range(y - 1, -1, -1):
-            if (x, j) not in self.board or self.board[(x, j)] != player:
-                break
-            num += 1
-        if num >= self.N:
-            return True
+        s = ''
+        if blocked >= 2 or num <= 1:
+            return
+        if blocked == 0:
+            s = 'open ' + str(num)
+        elif blocked == 1:
+            s = 'blocked ' + str(num)
 
-        # Diagonal /
-        num = 1
-        for i in range(1, self.boardSize):
-            piece = (x + i, y + i)
-            if not self.withinBounds(piece) or piece not in self.board or self.board[piece] != player:
-                break
-            num += 1
-        for i in range(1, self.boardSize):
-            piece = (x - i, y - i)
-            if not self.withinBounds(piece) or piece not in self.board or self.board[piece] != player:
-                break
-            num += 1
-        if num >= self.N:
-            return True
-
-        # Diagonal \
-        num = 1
-        for i in range(1, self.boardSize):
-            piece = (x + i, y - i)
-            if not self.withinBounds(piece) or piece not in self.board or self.board[piece] != player:
-                break
-            num += 1
-        for i in range(1, self.boardSize):
-            piece = (x - i, y + i)
-            if not self.withinBounds(piece) or piece not in self.board or self.board[piece] != player:
-                break
-            num += 1
-        if num >= self.N:
-            return True
-
-        return False
-
+        feature = (player, s)
+        util.addItemToDict(self.features, feature)
+    
--- util.py
+++ util.py
@@ -6,3 +6,21 @@ def isInt(str):
 		return True
 	except:
 		return False
+
+# Add an item to the given dictionary
+# d is a dict of (item => number of item) pairs
+def addItemToDict(d, item):
+    if item in d:
+        d[item] += 1
+    else:
+        d[item] = 1
+
+# Add an item to the given dictionary
+# d is a dict of (item => number of item) pairs
+# It is an error if d does not contain item
+def deleteItemFromDict(d, item):
+    assert (item in d), 'Dictionary does not contain the item(' + str(item) + ') to be deleted.'
+    if d[item] > 1:
+        d[item] -= 1
+    else:
+        del d[item]
